cbdef11dbfa4dcd315c14f9fbdef92376e65b02e
Jerome <jerome.proost@checkiz.com>
2014-04-01 14:49:28 +0200

    bson to json

---
+++ test/bsonjson_test.exs
@@ -0,0 +1,25 @@
+Code.require_file "test_helper.exs", __DIR__
+
+defmodule BsonJson.Test do
+  use ExUnit.Case
+
+  test "Stringify" do
+  	assert {"{}", ""} = BsonJson.stringify(Bson.encode(%{}))
+  	assert {"{\"a\":1}", ""} = BsonJson.stringify(Bson.encode(%{a: 1}))
+  	assert {"{\"a\":1.1}", ""} = BsonJson.stringify(Bson.encode(%{a: 1.1}))
+  	assert {"{\"a\":\"ab\"}", ""} = BsonJson.stringify(Bson.encode(%{a: "ab"}))
+  	assert {"{\"a\":\"a\",\"b\":\"b\"}", ""} = BsonJson.stringify(Bson.encode(%{a: "a", b: "b"}))
+  	assert {"{\"a\":[1,2]}", ""} = BsonJson.stringify(Bson.encode(%{a: [1,2]}))
+  end
+
+  test "ObjectId" do
+    term = Bson.ObjectId[oid: <<82, 224, 252, 230, 0, 0, 2, 0, 3, 0, 0, 4>>]
+    bson = Bson.encode(%{"0": term})
+
+    assert {"{\"0\":\"52e0fce60000020003000004\"}", ""} == BsonJson.stringify(bson)
+  end
+  test "dono" do
+  	bson = <<27, 0, 0, 0, 2, 85, 110, 105, 113, 117, 101, 67, 97, 114, 114, 105, 101, 114, 0, 3, 0, 0, 0, 87, 78, 0, 0>>
+  end
+
+end
