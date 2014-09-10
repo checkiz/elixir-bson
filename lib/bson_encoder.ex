845d0d7be140f5e88351b20daa2b25958dc229f1
Jerome <jerome.proost@checkiz.com>
2014-09-10 21:20:53 +0200

    improve error message in encoder

--- lib/bson_encoder.ex
+++ lib/bson_encoder.ex
@@ -29,24 +29,17 @@ defprotocol BsonEncoder do
 end
 
 defimpl BsonEncoder, for: Integer do
-
-  def encode(i, name), do: pre(i) <> name <> <<0x00>> <> int(i)
-
-  defp int(i) when -0x80000000 <= i and i <= 0x80000000, do: Bson.int32(i)
-  defp int(i) when -0x8000000000000000 <= i and i <= 0x8000000000000000, do: Bson.int64(i)
-
-  defp pre(i) when -0x80000000 <= i and i <= 0x80000000, do: <<0x10>>
-  defp pre(i) when -0x8000000000000000 <= i and i <= 0x8000000000000000, do: <<0x12>>
+  def encode(i, name) when -0x80000000 <= i and i <= 0x80000000, do: <<0x10>> <> name <> <<0x00>> <> <<i::32-signed-little>>
+  def encode(i, name) when -0x8000000000000000 <= i and i <= 0x8000000000000000, do: <<0x12>> <> name <> <<0x00>> <> <<i::64-signed-little>>
+  def encode(i, name), do: {:error, {"cannot encode integer", name, i}}
 end
 
 defimpl BsonEncoder, for: Float do
-  def encode(f, name), do: <<0x01>> <> name <> <<0x00>> <> float(f)
-
-  defp float(f),   do: <<(f)::size(64)-float-little>>
+  def encode(f, name), do: <<0x01>> <> name <> <<0x00>> <> <<(f)::size(64)-float-little>>
 end
 
 defimpl BsonEncoder, for: Atom do
-  # encode pair where value is an atom
+  # predefind Bson value
   def encode(false, name),   do: <<0x08>> <> name <> <<0x00>> <> <<0x00>>
   def encode(true, name),    do: <<0x08>> <> name <> <<0x00>> <> <<0x01>>
   def encode(nil, name),     do: <<0x0a>> <> name <> <<0x00>>
@@ -55,7 +48,8 @@ defimpl BsonEncoder, for: Atom do
   def encode(:'-inf', name), do: <<0x01>> <> name <> <<0x00>> <> <<0, 0, 0, 0, 0, 0, 240, 255>>
   def encode(MIN_KEY, name), do: <<0xff>> <> name <> <<0x00>>
   def encode(MAX_KEY, name), do: <<0x7f>> <> name <> <<0x00>>
-  def encode(atom, name),    do: <<0x0e>> <> name <> <<0x00>> <> Bson.string(Atom.to_string(atom))
+  # other Elixir atom are encoded like strings ()
+  def encode(atom, name),    do: <<0x0e>> <> name <> <<0x00>> <> (atom |> Atom.to_string |> Bson.string)
 end
 
 defimpl BsonEncoder, for: Regex do
@@ -63,9 +57,8 @@ defimpl BsonEncoder, for: Regex do
 end
 
 defimpl BsonEncoder, for: Bson.ObjectId do
-  def encode(%Bson.ObjectId{oid: oid}, name) when is_binary(oid) do
-    <<0x07>> <> name <> <<0x00>> <> oid
-  end
+  def encode(%Bson.ObjectId{oid: oid}, name) when is_binary(oid), do: <<0x07>> <> name <> <<0x00>> <> oid
+  def encode(obj, name), do: {:error, {"cannot encode object id", name, obj}}
 end
 
 defimpl BsonEncoder, for: Bson.JS do
@@ -73,65 +66,70 @@ defimpl BsonEncoder, for: Bson.JS do
     <<0x0d>> <> name <> <<0x00>> <> Bson.string(js)
   end
   def encode(%Bson.JS{code: js, scope: ctx}, name) when is_binary(js) and is_map(ctx) do
-    <<0x0f>> <> name <> <<0x00>> <> js_ctx(Bson.string(js) <> BsonEncoder.Map.encode_e_list(ctx))
+    case BsonEncoder.Map.encode(ctx, "") do
+      {:error, reason} ->
+        {:error, {"cannot encode context of js", name, reason}}
+      <<_::16, ctxBin::binary>> ->
+        <<0x0f>> <> name <> <<0x00>> <> js_ctx(Bson.string(js) <> ctxBin)
+    end
   end
+  def encode(js, name), do: {:error, "cannot encode '#{name}' : #{inspect(js)}"}
 
   defp js_ctx(jsctx), do: Bson.int32(byte_size(jsctx)+4) <> jsctx
 end
 
 defimpl BsonEncoder, for: Bson.Bin do
-  def encode(%Bson.Bin{bin: bin, subtype: subtype}, name), do:  <<0x05>> <> name <> <<0x00>> <> Bson.int32(byte_size(bin)) <> subtype <> bin
+  def encode(%Bson.Bin{bin: bin, subtype: subtype}, name)
+    when is_binary(bin) and is_binary(subtype) and byte_size(subtype) == 1,
+    do:  <<0x05>> <> name <> <<0x00>> <> Bson.int32(byte_size(bin)) <> subtype <> bin
+  def encode(bin, name), do: {"cannot encode binary", name, bin}
 end
 
 defimpl BsonEncoder, for: Bson.Timestamp do
-  def encode(%Bson.Timestamp{inc: i, ts: t}, name),        do:  <<0x11>> <> name <> <<0x00>> <> Bson.int32(i) <> Bson.int32(t)
+  def encode(%Bson.Timestamp{inc: i, ts: t}, name)
+    when is_integer(i) and -0x80000000 <= i and i <= 0x80000000
+     and is_integer(t) and -0x80000000 <= t and t <= 0x80000000,
+    do: <<0x11>> <> name <> <<0x00, i::32-signed-little, t::32-signed-little>>
+    def encode(ts, name), do: {"cannot encode timestamp", name, ts}
 end
 
 defimpl BsonEncoder, for: BitString do
   def encode(s, name) when is_binary(s),  do: <<0x02>> <> name <> <<0x00>> <> Bson.string(s)
+  def encode(bits, name), do: {:error, {"cannot encode non-binary bitsrings", name, bits}}
 end
 
 defimpl BsonEncoder, for: List do
-  # def encode([], name),                  do:  <<0x03>> <> name <> <<0x00>> <> Bson.doc(<<>>)
-  def encode(array, name) when is_list(array) do
-    <<0x04>> <> name <> <<0x00>> <> encode_array(array)
+  def encode(array, name) do
+    case Enumerable.reduce(array, {:cont, {[], 0}},
+      fn(item, {bufferAcc, i}) ->
+        name = i |> Integer.to_string
+        case BsonEncoder.encode(item, name) do
+          {:error, reason} -> {:halt, {"cannot encode item in array", name, reason}}
+          encoded -> {:cont, {[encoded | bufferAcc], i+1}}
+        end
+      end) do
+      {:halted, reason} ->
+        {:error, reason}
+      {:done, {bufferAcc, _}} ->
+        <<0x04>> <> name <> <<0x00>> <> (bufferAcc |> Enum.reverse |> IO.iodata_to_binary |> Bson.doc)
+    end
   end
-
-  defp encode_array(arr) do
-    {_, arrbin } = arr
-      |> Enum.reduce({0, []}, fn(item, {n, acc}) ->
-          {n+1, [BsonEncoder.encode(item, n |> Integer.to_string) | acc]}
-        end)
-    arrbin |> bitlist_to_bsondoc
-  end
-
-  defp bitlist_to_bsondoc(arrbin), do: arrbin |> Enum.reverse |> IO.iodata_to_binary |> Bson.doc
-
 end
 
 defimpl BsonEncoder, for: Map do
-  # def encode(%{}, name),                  do:  <<0x03>> <> name <> <<0x00>> <> Bson.doc(<<>>)
   def encode(map, name) do
-    <<0x03>> <> name <> <<0x00>> <> encode_e_list(map)
-  end
-
-  @doc """
-  encode e_list, this is, concatenation of encoded element
-  """
-  def encode_e_list(map) do
-    :maps.fold( fn
-      k, v, acc when is_atom(k) -> [BsonEncoder.encode(v, k |> Atom.to_string)|acc]
-      k, v, acc -> [BsonEncoder.encode(v, Bson.encode(k))|acc]
-    end, [], map)
-      |> bitlist_to_bsondoc
-  end
-
-  defp bitlist_to_bsondoc(arrbin), do: arrbin |> Enum.reverse |> IO.iodata_to_binary |> Bson.doc
-
-end
-
-defimpl BsonEncoder, for: Any do
-  def encode(%{__struct__: _struct}=map, name) do
-    <<0x03>> <> name <> <<0x00>> <> BsonEncoder.Map.encode_e_list(map)
+    case Enumerable.reduce(map, {:cont, []},
+      fn({key, val}, bufferAcc) ->
+        name = if is_atom(key), do: Atom.to_string(key), else: key
+        case BsonEncoder.encode(val, name) do
+          {:error, reason} -> {:halt, {"cannot encode element in array", name, reason}}
+          encoded -> {:cont, [encoded | bufferAcc]}
+        end
+      end) do
+      {:halted, reason} ->
+        {:error, reason}
+      {:done, bufferAcc} ->
+        <<0x03>> <> name <> <<0x00>> <> (bufferAcc |> Enum.reverse |> IO.iodata_to_binary |> Bson.doc)
+    end
   end
 end
