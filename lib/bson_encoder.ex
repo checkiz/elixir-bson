d110316c2067af1a2a5340164a83e556b560f320
Jerome <jerome.proost@checkiz.com>
2014-09-10 15:58:41 +0200

    change format of hex values

--- lib/bson_encoder.ex
+++ lib/bson_encoder.ex
@@ -31,32 +31,32 @@ end
 
 defimpl BsonEncoder, for: Integer do
 
-  def encode(i, name), do: pre(i) <> name <> "\x00" <> int(i)
+  def encode(i, name), do: pre(i) <> name <> <<0x00>> <> int(i)
 
   defp int(i) when -0x80000000 <= i and i <= 0x80000000, do: Bson.int32(i)
   defp int(i) when -0x8000000000000000 <= i and i <= 0x8000000000000000, do: Bson.int64(i)
 
-  defp pre(i) when -0x80000000 <= i and i <= 0x80000000, do: "\x10"
-  defp pre(i) when -0x8000000000000000 <= i and i <= 0x8000000000000000, do: "\x12"
+  defp pre(i) when -0x80000000 <= i and i <= 0x80000000, do: <<0x10>>
+  defp pre(i) when -0x8000000000000000 <= i and i <= 0x8000000000000000, do: <<0x12>>
 end
 
 defimpl BsonEncoder, for: Float do
-  def encode(f, name), do: "\x01" <> name <> "\x00" <> float(f)
+  def encode(f, name), do: <<0x01>> <> name <> <<0x00>> <> float(f)
 
   defp float(f),   do: <<(f)::size(64)-float-little>>
 end
 
 defimpl BsonEncoder, for: Atom do
   # encode pair where value is an atom
-  def encode(false, name),   do: "\x08" <> name <> "\x00" <> "\x00"
-  def encode(true, name),    do: "\x08" <> name <> "\x00" <> "\x01"
-  def encode(nil, name),     do: "\x0a" <> name <> "\x00"
-  def encode(:nan, name),    do: "\x01" <> name <> "\x00" <> <<0, 0, 0, 0, 0, 0, 248, 127>>
-  def encode(:'+inf', name), do: "\x01" <> name <> "\x00" <> <<0, 0, 0, 0, 0, 0, 240, 127>>
-  def encode(:'-inf', name), do: "\x01" <> name <> "\x00" <> <<0, 0, 0, 0, 0, 0, 240, 255>>
-  def encode(MIN_KEY, name), do: <<0xff>> <> name <> "\x00"
-  def encode(MAX_KEY, name), do: "\x7f" <> name <> "\x00"
-  def encode(atom, name),    do: "\x0e" <> name <> "\x00" <> Bson.string(Atom.to_string(atom))
+  def encode(false, name),   do: <<0x08>> <> name <> <<0x00>> <> <<0x00>>
+  def encode(true, name),    do: <<0x08>> <> name <> <<0x00>> <> <<0x01>>
+  def encode(nil, name),     do: <<0x0a>> <> name <> <<0x00>>
+  def encode(:nan, name),    do: <<0x01>> <> name <> <<0x00>> <> <<0, 0, 0, 0, 0, 0, 248, 127>>
+  def encode(:'+inf', name), do: <<0x01>> <> name <> <<0x00>> <> <<0, 0, 0, 0, 0, 0, 240, 127>>
+  def encode(:'-inf', name), do: <<0x01>> <> name <> <<0x00>> <> <<0, 0, 0, 0, 0, 0, 240, 255>>
+  def encode(MIN_KEY, name), do: <<0xff>> <> name <> <<0x00>>
+  def encode(MAX_KEY, name), do: <<0x7f>> <> name <> <<0x00>>
+  def encode(atom, name),    do: <<0x0e>> <> name <> <<0x00>> <> Bson.string(Atom.to_string(atom))
 end
 
 defimpl BsonEncoder, for: Regex do
@@ -65,27 +65,27 @@ end
 
 defimpl BsonEncoder, for: Bson.ObjectId do
   def encode(%Bson.ObjectId{oid: oid}, name) when is_binary(oid) do
-    "\x07" <> name <> "\x00" <> oid
+    <<0x07>> <> name <> <<0x00>> <> oid
   end
 end
 
 defimpl BsonEncoder, for: Bson.JS do
   def encode(%Bson.JS{code: js, scope: nil}, name) when is_binary(js) do
-    "\x0d" <> name <> "\x00" <> Bson.string(js)
+    <<0x0d>> <> name <> <<0x00>> <> Bson.string(js)
   end
   def encode(%Bson.JS{code: js, scope: ctx}, name) when is_binary(js) and is_map(ctx) do
-    "\x0f" <> name <> "\x00" <> js_ctx(Bson.string(js) <> BsonEncoder.Map.encode_e_list(ctx))
+    <<0x0f>> <> name <> <<0x00>> <> js_ctx(Bson.string(js) <> BsonEncoder.Map.encode_e_list(ctx))
   end
 
   defp js_ctx(jsctx), do: Bson.int32(byte_size(jsctx)+4) <> jsctx
 end
 
 defimpl BsonEncoder, for: Bson.Bin do
-  def encode(%Bson.Bin{bin: bin, subtype: subtype}, name), do:  "\x05" <> name <> "\x00" <> Bson.int32(byte_size(bin)) <> subtype <> bin
+  def encode(%Bson.Bin{bin: bin, subtype: subtype}, name), do:  <<0x05>> <> name <> <<0x00>> <> Bson.int32(byte_size(bin)) <> subtype <> bin
 end
 
 defimpl BsonEncoder, for: Bson.Timestamp do
-  def encode(%Bson.Timestamp{inc: i, ts: t}, name),        do:  "\x11" <> name <> "\x00" <> Bson.int32(i) <> Bson.int32(t)
+  def encode(%Bson.Timestamp{inc: i, ts: t}, name),        do:  <<0x11>> <> name <> <<0x00>> <> Bson.int32(i) <> Bson.int32(t)
 end
 
 defimpl BsonEncoder, for: Tuple do
--- lib/bson_tokenizer.ex
+++ lib/bson_tokenizer.ex
@@ -122,56 +122,56 @@ defmodule BsonTk do
   `BsonTk.Bool`, `BsonTk.ObjectId`, `BsonTk.Bin`, `BsonTk.Regex`, `BsonTk.JS`, `BsonTk.Now`, `BsonTk.Timestamp`)
   * `tk_end` - is the end position of the element
   """
-  def tokenize_element("\x10", _, from), do: {%Int32{part: {from, 4}}, from+4}
-  def tokenize_element("\x12", _, from), do: {%Int64{part: {from, 8}}, from+8}
-  def tokenize_element("\x01", _, from), do: {%Float{part: {from, 8}}, from+8}
-  def tokenize_element("\x07", _, from), do: {%ObjectId{part: {from, 12}}, from+12}
-  def tokenize_element("\x08", _, from), do: {%Bool{part: {from, 1}}, from+1}
-  def tokenize_element("\x09", _, from), do: {%Now{part: {from, 8}}, from+8}
-  def tokenize_element("\x06", _, from), do: {nil, from+0}
-  def tokenize_element("\x0a", _, from), do: {nil, from+0}
-  def tokenize_element("\x11", _, from), do: {%Timestamp{inc: {from, 4}, ts: {from+4, 4}}, from+8}
-  def tokenize_element(<<0xff>>, _, from), do: {MIN_KEY, from+0}
-  def tokenize_element("\x7f", _, from), do: {MAX_KEY, from+0}
+  defp tokenize_element(<<0x10>>, _, from), do: {%Int32{part: {from, 4}}, from+4}
+  defp tokenize_element(<<0x12>>, _, from), do: {%Int64{part: {from, 8}}, from+8}
+  defp tokenize_element(<<0x01>>, _, from), do: {%Float{part: {from, 8}}, from+8}
+  defp tokenize_element(<<0x07>>, _, from), do: {%ObjectId{part: {from, 12}}, from+12}
+  defp tokenize_element(<<0x08>>, _, from), do: {%Bool{part: {from, 1}}, from+1}
+  defp tokenize_element(<<0x09>>, _, from), do: {%Now{part: {from, 8}}, from+8}
+  defp tokenize_element(<<0x06>>, _, from), do: {nil, from+0}
+  defp tokenize_element(<<0x0a>>, _, from), do: {nil, from+0}
+  defp tokenize_element(<<0x11>>, _, from), do: {%Timestamp{inc: {from, 4}, ts: {from+4, 4}}, from+8}
+  defp tokenize_element(<<0xff>>, _, from), do: {MIN_KEY, from+0}
+  defp tokenize_element(<<0x7f>>, _, from), do: {MAX_KEY, from+0}
 
   # string
-  def tokenize_element("\x02", bson, from) do
+  defp tokenize_element(<<0x02>>, bson, from) do
     len = Bson.int32(bson, from)-1
     {%String{part: {from+4, len}}, from+len+4+1}
   end
 
   # atom
-  def tokenize_element("\x0e", bson, from) do
+  defp tokenize_element(<<0x0e>>, bson, from) do
     len = Bson.int32(bson, from)-1
     {%Atom{part: {from+4, len}}, from+len+4+1}
   end
 
   # document
-  def tokenize_element("\x03", bson, from) do
+  defp tokenize_element(<<0x03>>, bson, from) do
     len = Bson.int32(bson, from)-5
     {%Doc{part: {from+4, len}}, from+len+4+1}
   end
 
   # array
-  def tokenize_element("\x04", bson, from) do
+  defp tokenize_element(<<0x04>>, bson, from) do
     len = Bson.int32(bson, from)-5
     {%Array{part: {from+4, len}}, from+len+4+1}
   end
 
   # regex
-  def tokenize_element("\x0b", bson, from) do
+  defp tokenize_element(<<0x0b>>, bson, from) do
     to = byte_size(bson) -1
     patternend = Bson.peek_cstring_end(bson, from, to)
     optsend = Bson.peek_cstring_end(bson, patternend+1, to)
     {%BsonTk.Regex{pattern: {from, patternend-from}, opts: {patternend+1, optsend-(patternend+1)}}, optsend+1}
   end
   # javascript
-  def tokenize_element("\x0d", bson, from) do
+  defp tokenize_element(<<0x0d>>, bson, from) do
     len = Bson.int32(bson, from)-1
     {%BsonTk.JS{code: {from+4, len}}, from+len+4+1}
   end
   # javascript with scope
-  def tokenize_element("\x0f", bson, code_ws_from) do
+  defp tokenize_element(<<0x0f>>, bson, code_ws_from) do
     # code_ws_len = Bson.int32(bson, code_ws_from)-1    #code_w_s length
     codefrom = code_ws_from+4
     codelen = Bson.int32(bson, codefrom)-1    #string length
@@ -180,7 +180,7 @@ defmodule BsonTk do
     {%BsonTk.JS{code: {codefrom+4, codelen}, scope: {scopefrom+4, scopelen}}, scopefrom+scopelen+4+1}
   end
   # binary
-  def tokenize_element("\x05", bson, from) do
+  defp tokenize_element(<<0x05>>, bson, from) do
     len = Bson.int32(bson, from)
     {%BsonTk.Bin{part: {from+5, len}, subtype: binary_part(bson, from+4, 1)}, from+len+5}
   end
