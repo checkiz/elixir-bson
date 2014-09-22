0be9d4f5650c4711f056ae8243de35c8341a980d
Jerome <jerome.proost@checkiz.com>
2014-09-22 22:36:19 +0200

    fix release

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Bson.Mixfile do
   def project do
     [ app: :bson,
       name: "bson",
-      version: "0.3.1",
+      version: "0.4.0",
       elixir: "~> 1.0.0",
       description: "BSON implementation for Elixir",
       source_url: "https://github.com/checkiz/elixir-bson",
